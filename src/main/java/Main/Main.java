3dd2ea80ff542edb01f8d8d59e4c60edcdde3c0c
Andrea <autremonde621@gmail.com>
2026-04-14 14:04:22 -0600

    rama Andri - ya hice el DAO para equipo y tambien Main para  probar crud

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,29 @@
+
+package Main;
+package ClasesDAO.EquipoDAO;
+
+public class Main {
+    public static void main(String[] args) {
+        EquipoDAO dao = new EquipoDAO();
+
+        // 🔹 CREATE
+        Equipo nuevo = new Equipo(0, "Guatemala", "Guatemala");
+        dao.insertar(nuevo);
+        System.out.println("Insertado ✔");
+
+        // 🔹 READ
+        System.out.println("Lista de equipos:");
+        for (Equipo e : dao.obtenerTodos()) {
+            System.out.println(e);
+        }
+
+        // 🔹 UPDATE
+        Equipo actualizar = new Equipo(1, "Brasil", "Brasil");
+        dao.actualizar(actualizar);
+        System.out.println("Actualizado ✔");
+
+        // 🔹 DELETE
+        dao.eliminar(1);
+        System.out.println("Eliminado ✔");
+    }
+}
